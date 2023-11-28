8e5d5d03e392c7f1f2740c3e6523e3f1eff606cf
Agustin Macoggi <agustinmacoggi@gmail.com>
2023-11-28 00:01:12 -0300

    Upload proyecto (PUT/GET)

---
+++ Challenge/Controllers/UsuariosController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Challenge.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        // Instancio el objeto Usuario de su Clase.
+        Usuario usuario = new Usuario();
+
+        // Creo la lista a utilizar para guardar en memoria los usuarios
+        private static readonly List<Usuario> _usuarios = new List<Usuario>();
+
+        // Endpoint para crear un usuario
+        [HttpPost]
+        public IActionResult CrearUsuario(string username, string password)
+        {
+            usuario.Id = Guid.NewGuid();
+            usuario.Username = username;
+            usuario.Password = password;
+            usuario.PasswordEncriptada = password + " encriptado";
+            _usuarios.Add(usuario);
+
+            return Ok(usuario);
+        }
+        // Endpoint para obtener todos los usuarios
+        [HttpGet]
+        public IActionResult ObtenerTodosLosUsuarios() 
+        {
+            return Ok(_usuarios);
+        }
+
+        // Endpoint para buscar un usuario por nombre de usuario
+        [HttpGet("{username}")]
+        public IActionResult ObtenerUsuario(string username)
+        {
+            // Busco un usuario según su username (LINQ) y lo guardo en la var. Si no encuentra nada, se
+            // guarda null y se muestra un mensaje.
+            var usuario = _usuarios.Where(x => x.Username == username).FirstOrDefault();
+
+            if (usuario == null)
+            {
+                return NotFound("Usuario no encontrado");
+            }
+
+            return Ok(usuario);
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+<h1 align="center"> Challenge NVX - Gaia </h1>
+
+Podes ver las consignas del challenge ingresando en este [link!](https://drive.google.com/file/d/1QNxcS9G3iWRDVDDBwJuc32UPNgufhDVn/view)
+
+# Tiempo total de desarrollo: ?
+- 26/11/23 -> 1 Hora
+- 27/11/23 -> 3 Horas
+
+# Conceptos buscados: x
+
+# 
+![logo-am](https://github.com/AguzKind/aguzkind.github.io/assets/105683402/f6e227dd-dc65-452c-8cec-936962e43f96)
