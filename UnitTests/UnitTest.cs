6a125adac79d96dc542f0ad618006148f5d0b0ab
Agustin Macoggi <agustinmacoggi@gmail.com>
2023-11-29 16:42:21 -0300

    Fix UnitTests

--- UnitTests/UnitTest.cs
+++ UnitTests/UnitTest.cs
@@ -1,3 +1,5 @@
+using System.Net;
+
 namespace UnitTests
 {
     public class Tests
@@ -6,17 +8,26 @@ namespace UnitTests
         public void Setup()
         {
         }
+        Challenge.Controllers.UsuariosController _usuariosController = new Challenge.Controllers.UsuariosController();
         Encriptacion _encriptacion = new Encriptacion();
         Desencriptacion _desencriptacion = new Desencriptacion();
 
         [Order(0)]
+        [Test]
+        public void TestPost()
+        {
+            _usuariosController.CrearUsuario("usuario1", "contraseña1");
+            Assert.Pass("El usuario se agrego con exito");
+        }
+
+        [Order(1)]
         [Test]
         public void TestEncriptacion()
         {
             string resultado = _encriptacion.Encriptar("holacomoestas");
             Assert.That(resultado, Is.EqualTo("se$ts$iu$mu$ce$lu$h"));
         }
-        [Order(1)]
+        [Order(2)]
         [Test]
         public void TestDesencriptacion()
         {
